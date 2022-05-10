f8de2ff192d3ad745b09effd7f71312858c3fcc1
amexiasd <amexias@outlook.com>
2022-05-10 22:06:57 +0300

    Random Number

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function printAll(name) {
     console.log("".concat(name, " called this function"));
 }
+function random(num) {
+    var rand = Math.floor(Math.random() * num);
+    return rand;
+}
 function main() {
     console.log('Main Function');
     printAll('Mex');
+    console.log(random(19));
 }
 main();
--- script.ts
+++ script.ts
@@ -2,9 +2,14 @@ function printAll(name: string) {
 	console.log(`${name} called this function`);
 }
 
+function random(num: number) {
+	let rand = Math.floor(Math.random() * num);
+	return rand;
+}
 function main() {
 	console.log('Main Function');
 	printAll('Mex');
+	console.log(random(19));
 }
 
 main();
