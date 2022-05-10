439eba08893d0a8c62b08ea8b5814f463d4bd80d
amexiasd <amexias@outlook.com>
2022-05-10 21:44:08 +0300

    buzz fizz

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-console.log(12 + 2);
-console.log(123);
-console.log(123);
+var fizz = 3;
+var buzz = 5;
+var num = 235;
+var i = 0;
+while (i <= num) {
+    if (i % fizz == 0 && i % buzz == 0) {
+        console.log('FizzBuzz');
+    }
+    else {
+        if (i % fizz == 0) {
+            console.log('fizz');
+        }
+        else {
+            if (i % buzz == 0) {
+                console.log('Buzz');
+            }
+            else {
+                console.log(i);
+            }
+        }
+    }
+    i += 1;
+}
