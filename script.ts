204ed7ce63dbf2fd4ae53570bc4b7f4aa3510f5f
amexiasd <amexias@outlook.com>
2022-05-10 21:43:32 +0300

    BuzzFizz

--- script.ts
+++ script.ts
@@ -1,3 +1,23 @@
-console.log(12 + 2);
-console.log(123);
-console.log(123);
+let fizz = 3;
+let buzz = 5;
+
+const num = 235;
+
+let i = 0;
+
+while (i <= num) {
+	if (i % fizz == 0 && i % buzz == 0) {
+		console.log('FizzBuzz');
+	} else {
+		if (i % fizz == 0) {
+			console.log('fizz');
+		} else {
+			if (i % buzz == 0) {
+				console.log('Buzz');
+			} else {
+				console.log(i);
+			}
+		}
+	}
+	i += 1;
+}
