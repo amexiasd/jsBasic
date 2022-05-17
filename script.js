c97ba8104707b3895727567a45ab08ac9006d72d
amexiasd <amexias@outlook.com>
2022-05-17 12:14:02 +0300

    player class

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var a = 12;
-var b = "222";
-console.log(23 + 97);
-console.log(1 + 2 + 3 + 4 + 5 + 6);
-console.log((4 + 6 + 9) / 77);
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+var player = /** @class */ (function () {
+    function player(times, power) {
+        this.plays = [];
+        this.times = times;
+        this.power = power;
+        this.sum = 0;
+    }
+    player.prototype.play1 = function () {
+        for (var i = 0; i < this.times; i++) {
+            var r = rand(this.power, this.power + 50);
+            this.plays.push(r);
+        }
+        this.sum = this.plays.reduce(function (a, b) {
+            return a + b;
+        });
+        console.log(this.sum);
+    };
+    return player;
+}());
+var players = [];
+for (var i = 0; i < 40; i++) {
+    players[i] = new player(15, i + 100);
+}
+for (var j = 0; j < 40; j++) {
+    players[j].play1();
+}
--- script.ts
+++ script.ts
@@ -1,5 +1,43 @@
-let a = 12;
-let b = "222"
-console.log(23 + 97);
-console.log(1 + 2 + 3 + 4 + 5 + 6);
-console.log((4 + 6 + 9) / 77);
+function rand(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+
+}
+
+class player {
+    plays: number[];
+    times: number;
+    power: number;
+    sum: number;
+
+    constructor(times, power) {
+
+
+        this.plays = [];
+        this.times = times;
+        this.power = power;
+        this.sum = 0;
+    }
+
+    play1() {
+        for (let i = 0; i < this.times; i++) {
+            let r = rand(this.power, this.power + 50);
+            this.plays.push(r);
+
+        }
+
+        this.sum = this.plays.reduce(function (a, b) {
+            return a + b;
+        });
+        console.log(this.sum);
+    }
+
+}
+
+let players = [];
+for (let i = 0; i < 40; i++) {
+    players[i] = new player(15, i + 100);
+
+}
+for (let j = 0; j < 40; j++) {
+    players[j].play1();
+}
